1ec72f1cfa21dcd18200f749e720ad75b3be4993
a118skr <98032893+a118skr@users.noreply.github.com>
2022-05-11 14:05:17 +0000

    [Add]投稿、一覧、更新、削除作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get '/books' => 'books#index'
+  get 'books' => 'books#index'
   post 'books' => 'books#create' #index画面から新規投稿を送る
 
-  get 'books/show'
-  get 'books/edit'
+  get 'books/:id' => 'books#show',as: 'book' #名前ルート指定　indexからリンク作成
+
+  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
+  patch 'books/:id' => 'books#update',as: 'update_book'
+
+  delete 'books/:id' => 'books#destroy',as: 'destroy_book'
 
 
   root to: 'homes#top'
