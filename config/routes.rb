8b0203c411f8101c12617ee654af485e73e95cab
a118skr <98032893+a118skr@users.noreply.github.com>
2022-05-11 08:58:19 +0000

    [Finish]投稿フォーム作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'books/index'
+  get '/books' => 'books#index'
+  post 'books' => 'books#create' #index画面から新規投稿を送る
+
   get 'books/show'
   get 'books/edit'
-  get 'homes/top'
+
+
+  root to: 'homes#top'
+
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
