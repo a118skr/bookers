bae837ae15936a0e385254d52d91b3af3d4be021
a118skr <98032893+a118skr@users.noreply.github.com>
2022-05-14 12:17:44 +0000

    [Finish]3-12

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,15 +1,21 @@
 class BooksController < ApplicationController
-  def index #新規投稿createアクションもある
-    @book = Book.new
+  def index
+    @book = Book.new #indexページに空の箱
     @books = Book.all
 
   end
 
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params) 
+
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
 
@@ -24,13 +30,19 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)
+    @book = Book.find(params[:id])
+
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
 
   def destroy
     book = Book.find(params[:id])
+    flash[:notice] = "Book was successfully destroyed."
     book.destroy
     redirect_to '/books'
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,8 @@
 Rails.application.routes.draw do
-  get 'books' => 'books#index'
-  post 'books' => 'books#create' #index画面から新規投稿を送る
-
-  get 'books/:id' => 'books#show',as: 'book' #名前ルート指定　indexからリンク作成
-
-  get 'books/:id/edit' => 'books#edit',as: 'edit_book'
-  patch 'books/:id' => 'books#update',as: 'update_book'
-
-  delete 'books/:id' => 'books#destroy',as: 'destroy_book'
-
 
   root to: 'homes#top'
 
+  resources :books
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
